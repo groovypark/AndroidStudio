9453ee0dd57171a06a0c0d0c01a2876f3b117239
unknown <soobin950@gmail.com>
2017-09-12 19:09:58 +0900

    Make button

---
+++ MyApplication/app/src/main/java/com/example/soobi/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.soobi.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.button);
+
+        View.OnClickListener listener = new  View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "my test",
+                    Toast.LENGTH_SHORT).show();
+            }
+        };
+        btn.setOnClickListener(listener);
+    }
+}
